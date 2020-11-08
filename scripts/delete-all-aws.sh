ce486dbcf3ceae2f56c66f121354de14dd897eba
Chuck D'Antonio <cdantonio@pivotal.io>
2020-11-08 14:09:57 -0500

    Adapts for new TKG CLI required args

--- scripts/delete-all-aws.sh
+++ scripts/delete-all-aws.sh
@@ -5,12 +5,20 @@ source $TKG_LAB_SCRIPTS/set-env.sh
 
 VMWARE_ID=$(yq r $PARAMS_YAML vmware-id)
 
-tmc cluster delete $VMWARE_ID-$(yq r $PARAMS_YAML management-cluster.name)-$(yq r $PARAMS_YAML iaas) --force
-tmc cluster delete $VMWARE_ID-$(yq r $PARAMS_YAML shared-services-cluster.name)-$(yq r $PARAMS_YAML iaas) --force
+# tmc cluster delete -m attached -p attached $VMWARE_ID-$(yq r $PARAMS_YAML management-cluster.name)-$(yq r $PARAMS_YAML iaas) --force
+# tmc cluster delete -m attached -p attached $VMWARE_ID-$(yq r $PARAMS_YAML shared-services-cluster.name)-$(yq r $PARAMS_YAML iaas) --force
 
-tmc cluster namespace delete acme-fitness $VMWARE_ID-$(yq r $PARAMS_YAML workload-cluster.name)-$(yq r $PARAMS_YAML iaas)
-tmc workspace delete $(yq r $PARAMS_YAML acme-fitness.tmc-workspace)
-tmc cluster delete $VMWARE_ID-$(yq r $PARAMS_YAML workload-cluster.name)-$(yq r $PARAMS_YAML iaas) --force
+set +e
+if tmc cluster namespace get -p attached -m attached --cluster-name $VMWARE_ID-$(yq r $PARAMS_YAML workload-cluster.name)-$(yq r $PARAMS_YAML iaas) acme-fitness 2>&1 > /dev/null; then 
+	tmc cluster namespace delete -m attached -p attached --cluster-name $VMWARE_ID-$(yq r $PARAMS_YAML workload-cluster.name)-$(yq r $PARAMS_YAML iaas) acme-fitness 
+fi
+
+if tmc workspace get $(yq r $PARAMS_YAML acme-fitness.tmc-workspace) 2>&1 > /dev/null ; then
+	tmc workspace delete $(yq r $PARAMS_YAML acme-fitness.tmc-workspace)
+fi
+set -e 
+
+tmc cluster delete -m attached -p attached $VMWARE_ID-$(yq r $PARAMS_YAML workload-cluster.name)-$(yq r $PARAMS_YAML iaas) --force
 
 tkg set mc $(yq r $PARAMS_YAML management-cluster.name)
 tkg delete cluster $(yq r $PARAMS_YAML workload-cluster.name) --yes
